453eb4a5d357cad59a7059568ca3aa075b579df7
Adhitia Nugraha <adhitia.nugrahatkj@gmail.com>
2024-03-28 15:11:44 +0700

    fix host production bug

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 
 const init = async () => {
     const server = hapi.Server({
-        host: 'localhost',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         port: 6969,
         routes: {
             cors: {
